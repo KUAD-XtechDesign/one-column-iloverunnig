19e39979159a8e6ea3bda4dda31c8897e73571a6
iloverunnig <68231939+iloverunnig@users.noreply.github.com>
2020-10-08 18:02:45 +0900

    dekinai

--- work/js/script.js
+++ work/js/script.js
@@ -1,16 +1,18 @@
 $(function(){
     
-    $(".img_button").on("click",function(){
-    let target = $(this).siblings(".content");
-    target.slideToggle("alow");
+ $(".img_button").on("click",function(){let target = $(this).siblings(".content");
+  target.slideToggle("alow");
+
+  $("html,body").animate({scrollTop:target.offset().top},"slow");)
+
+ })
+    
+    
    
-    $("html,body").animate({scrollTop:target.offset().top},"slow");
-   })
    
    $(".close_button").on("click",function(){
        $(this).parent(".content").slideup("slow");
-   
-   })
+    })
 
 
 });
