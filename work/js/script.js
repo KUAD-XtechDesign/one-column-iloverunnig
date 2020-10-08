3717f10e2cc58606481d968ea1645a82f84d6178
iloverunnig <68231939+iloverunnig@users.noreply.github.com>
2020-10-08 16:32:43 +0900

    dekuta?

---
+++ work/js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+    
+    $(".img_button").on("click",function(){
+    let target = $(this).siblings(".content");
+    target.slideToggle("alow");
+   
+    $("html,body").animate({scrollTop:target.offset().top},"slow");
+   })
+   
+   $(".close_button").on("click",function(){
+       $(this).parent(".content").slideup("slow");
+   
+   })
+
+
+});
+
+
+
